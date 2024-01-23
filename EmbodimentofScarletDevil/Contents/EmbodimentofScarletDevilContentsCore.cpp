737d9250772c11fe923bbcd1b6caa50ea7ffc8f5
Tabibito <cyh5343@naver.com>
2024-01-23 15:16:53 +0900

    Engine, Contents 코드 생성

---
+++ EmbodimentofScarletDevil/Contents/EmbodimentofScarletDevilContentsCore.cpp
@@ -0,0 +1,32 @@
+#include "EmbodimentofScarletDevilContentsCore.h"
+#include "Player.h"
+
+EmbodimentofScarletDevilContentsCore::EmbodimentofScarletDevilContentsCore()
+{
+}
+
+EmbodimentofScarletDevilContentsCore::~EmbodimentofScarletDevilContentsCore()
+{
+}
+
+// 게임시작
+void EmbodimentofScarletDevilContentsCore::EngineStart()
+{
+	//MainWindow.SetTitle();
+	//MainWindow.SetScale();
+
+	Player NewPlayer;
+
+	// 이때되면 이미 윈도우 창은 만들어져있는 상태일거라고 
+	int a = 0;
+}
+
+void EmbodimentofScarletDevilContentsCore::EngineUpdate()
+{
+	int a = 0;
+}
+
+void EmbodimentofScarletDevilContentsCore::EngineEnd()
+{
+	int a = 0;
+}
---
+++ EmbodimentofScarletDevil/Contents/EmbodimentofScarletDevilContentsCore.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <EngineCore\EngineCore.h>
+
+// Ό³Έν :
+class EmbodimentofScarletDevilContentsCore : public EngineCore
+{
+public:
+	// constrcuter destructer
+	EmbodimentofScarletDevilContentsCore();
+	~EmbodimentofScarletDevilContentsCore();
+
+	// delete Function
+	EmbodimentofScarletDevilContentsCore(const EmbodimentofScarletDevilContentsCore& _Other) = delete;
+	EmbodimentofScarletDevilContentsCore(EmbodimentofScarletDevilContentsCore&& _Other) noexcept = delete;
+	EmbodimentofScarletDevilContentsCore& operator=(const EmbodimentofScarletDevilContentsCore& _Other) = delete;
+	EmbodimentofScarletDevilContentsCore& operator=(EmbodimentofScarletDevilContentsCore&& _Other) noexcept = delete;
+
+protected:
+	void EngineStart() override;
+	void EngineUpdate() override;
+	void EngineEnd() override;
+
+private:
+
+};
+
