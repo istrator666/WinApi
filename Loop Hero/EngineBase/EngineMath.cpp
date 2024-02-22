4fc68e4cde6a4bec056d68f4f917be7404baa40d
최영환 <cyh5343@naver.com>
2024-02-23 00:40:37 +0900

    Sound 설정 Err 수정

--- Loop Hero/EngineBase/EngineMath.cpp
+++ Loop Hero/EngineBase/EngineMath.cpp
@@ -28,7 +28,7 @@ const Color8Bit Color8Bit::BlueA = { 0, 0, 255, 0 };
 const Color8Bit Color8Bit::YellowA = { 255, 255, 0, 255 };
 const Color8Bit Color8Bit::WhiteA = { 255, 255, 255, 0 };
 const Color8Bit Color8Bit::MagentaA = { 255, 0, 255, 0 };
-const Color8Bit Color8Bit::Orange = { 255, 170, 46, 255 };
+const Color8Bit Color8Bit::OrangeA = { 255, 170, 46, 255 };
 
 
 UEngineMath::UEngineMath()
